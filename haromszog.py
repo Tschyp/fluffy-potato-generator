da5e95a2aa1acf0f7a020208d76ae80cd6b5513e
nagyangelika90 <nagyangelika90@gmail.com>
2016-04-28 15:14:17 +0200

    Új feladat, csak a program törzs

---
+++ haromszog.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import random
+def haromszog():
+    szamok
+    for 
+    szamok.add = random.randint(2,15)
+
+
+    feladat = 'Megszerkeszthető-e a háromszög ' + szamok[1] +",...... oldalakból?'
+    
+    
+#valami ilyesmi kéne    
+    
+    
+    
+    return feladat, lehet, lehet.index(aktualis[jo])
+
+feladat, lehet, jo = haromszog()
+print(feladat)
+print(lehet)
+print('A helyes megoldás: '+lehet[jo])
