30c70959ef1085776d757731b2e1f663044d7340
Tschyp <imiimi@freemail.hu>
2016-05-14 09:41:17 +0200

    foprogram test v0

--- foprogram.py
+++ foprogram.py
@@ -4,9 +4,19 @@ Created on Thu May  5 14:26:59 2016
 
 @author: user
 """
-
+pontok = 0
 import haromszog
+
 
 feladat, megoldas = haromszog.haromszog()
 print(feladat)
 
+valasz=bool(raw_input('Megoldas:'))
+
+if valasz == megoldas:
+	print("Helyes!")
+	pontok = pontok + 1
+print(pontok)
+
+
+import ...	
